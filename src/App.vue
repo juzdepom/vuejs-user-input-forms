28d80de992a2f6ac0f88d51ab5f48b711003db19
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-25 05:06:33 -0800

    added isSubmitted property to toggle whether data panel below submit is shown.

--- src/App.vue
+++ src/App.vue
@@ -110,13 +110,17 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted"
+                            >Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row"
+        v-if="isSubmitted"
+        >
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -159,10 +163,16 @@
           selectedPriority: 'Medium',
           priorities: ['High', 'Medium', 'Low'],
           dataSwitch: true,
+          isSubmitted: false,
         }
       },
       components: {
         appSwitch: Switch,
+      },
+      methods: {
+        submitted(){
+          this.isSubmitted = true
+        }
       }
     }
 </script>
