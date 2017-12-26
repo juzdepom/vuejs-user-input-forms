7293fd72a0361499be7e25329b52fb35c623c3fb
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-26 07:06:56 -0800

    Instructor solution. He created a custom fullname vue object which was pretty complicated and worth looking at.

--- src/SignUp.vue
+++ src/SignUp.vue
@@ -1,71 +1,8 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                  <h1>Sign Up</h1>
-                  <hr>
-                  <div class="form-group">
-                      <label for="firstName">First Name</label>
-                      <input
-                              type="text"
-                              id="firstName"
-                              class="form-control"
-                              v-model="firstName"
-                              >
-
-                  </div>
-                  <div class="form-group">
-                      <label for="lastName">Last Name</label>
-                      <input
-                              type="text"
-                              id="lastName"
-                              class="form-control"
-                              v-model="lastName"
-                              >
-
-                  </div>
-                  <div class="form-group">
-                      <label for="password">Password</label>
-                      <input
-                              type="password"
-                              id="password"
-                              class="form-control"
-                              v-model.lazy="password"
-                              >
-                  </div>
-                  <div class="form-group">
-                      <label for="mail">Mail</label>
-                      <input
-                              type="text"
-                              id="mail"
-                              class="form-control"
-                              v-model="mail"
-                              >
-                  </div>
-                  <div class="form-group">
-                    <p>
-                      <strong>Store in Database?</strong>
-                    </p>
-                      <label for="yes">
-                          <input
-                                  type="radio"
-                                  id="yes"
-                                  value="Yes"
-                                  v-model="storeData"
-                                  > Yes
-                      </label>
-                      <label for="no">
-                          <input
-                                  style="margin-left:20px;"
-                                  type="radio"
-                                  id="no"
-                                  value="No"
-                                  v-model="storeData"
-                                  > No
-                      </label>
-                  </div>
-
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
@@ -73,35 +10,48 @@
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
 
-                    <!-- JULIA STOPPED HERE: NEXT DO BELOW  -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label>Mail</label>
+                        <input type="text" class="form-control" v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="radio" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+                    <button
+                            type="submit"
+                            @click.prevent="submitted = true"
+                            class="btn btn-primary">Submit the Form</button>
                 </div>
             </div>
-            <div class="text-center">
-              <button class="btn btn-primary"
-                      @click.prevent="submitted">Submit</button>
-            </div>
-
         </form>
         <hr>
-        <div v-if="isSubmitted" class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: {{fullName}}</p>
-                        <p>Mail: {{mail}}</p>
-                        <p>Password: {{password}}</p>
-                        <p>Store in Database? {{storeData}}</p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
                     </div>
                 </div>
             </div>
@@ -110,28 +60,21 @@
 </template>
 
 <script>
+    import FullName from './FullName.vue';
+
     export default {
-      data: function(){
-        return {
-          firstName: '',
-          lastName: '',
-          password: '',
-          mail: '',
-          storeData: '',
-          isSubmitted: false,
+        data() {
+            return {
+                fullName: 'Max Schwarzmüller',
+                email: '',
+                password: '',
+                storeData: 'Yes',
+                submitted: false
+            }
+        },
+        components: {
+            appFullName: FullName
         }
-      },
-      computed: {
-        fullName: function(){
-          console.log('working')
-          return (this.firstName + ' ' + this.lastName)
-        }
-      },
-      methods: {
-        submitted(){
-          this.isSubmitted = true;
-        }
-      }
     }
 </script>
 
